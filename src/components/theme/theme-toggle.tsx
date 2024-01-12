f3d10865df982c98387a0ab64479bb878946aa80
Connie <conniedpaula@gmail.com>
2024-01-12 11:20:00 -0300

    chore: improve theme button logic to allow system theme while still changing to correct theme on click, add theme button to auth pages

--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -1,22 +1,33 @@
 import { Moon, Sun } from "lucide-react";
 
 import { useTheme } from "@/components/theme/theme-provider";
-import { Button } from "@/components/ui/button";
+import { Button, ButtonProps } from "@/components/ui/button";
 
-export function ModeToggle() {
+export function ModeToggle(props: ButtonProps) {
   const { setTheme, theme } = useTheme();
+
+  const handleThemeButtonClick = () => {
+    let newTheme: "light" | "dark";
+    if (theme === "system") {
+      newTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "light"
+        : "dark";
+    } else {
+      newTheme = theme === "light" ? "dark" : "light";
+    }
+
+    setTheme(newTheme);
+  };
 
   return (
     <Button
       variant="outline"
       size="icon"
-      onClick={() => {
-        const newTheme = theme === "light" ? "dark" : "light";
-        setTheme(newTheme);
-      }}
+      onClick={handleThemeButtonClick}
+      {...props}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 
 import { Logo } from "@/components/logo";
+import { ModeToggle } from "@/components/theme/theme-toggle";
 
 export function AuthLayout() {
   return (
-    <div className="flex h-screen flex-col antialiased md:grid md:min-h-screen md:grid-cols-2">
+    <div className="relative flex h-screen flex-col antialiased md:grid md:min-h-screen md:grid-cols-2">
+      <ModeToggle className="absolute right-3 top-2.5" />
       <div className="flex h-fit flex-col justify-between border-r border-foreground/5 bg-muted p-4 text-muted-foreground md:h-full md:p-10">
         <div className="flex items-center gap-1 text-lg text-foreground md:gap-2">
           <Logo className="h-6 w-6 text-black" />
